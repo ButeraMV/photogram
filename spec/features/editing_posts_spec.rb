97f9adcfcd7ed45c738745cd9a7c063bcfba9690
Boots2291 <buteramv@gmail.com>
2019-04-11 13:51:19 -0400

    Add to editing posts spec

--- spec/features/editing_posts_spec.rb
+++ spec/features/editing_posts_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 feature 'Editing posts' do
-  scenario 'can edit a post' do
+  background do
     post = create(:post)
     visit '/'
     find(:xpath, "//a[contains(@href,'posts/#{post.id}')]").click
     click_link 'Edit Post'
+  end
+
+  scenario 'can edit a post' do
     fill_in 'Caption', with: 'Oops'
     click_button 'Update Post'
     expect(page).to have_content('Post updated')
     expect(page).to have_content('Oops')
+  end
+
+  it "won't update a post without an image" do
+    attach_file('Image', 'spec/files/coffee.zip')
+    click_button 'Update Post'
+
+    expect(page).to have_content("Something is wrong with your form")
   end
 end
